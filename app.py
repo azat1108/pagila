429036acf2fd79c03e19862b270fbff4f2578fd3
Azat Bilal <azat.bilal@alumni.nu.edu.kz>
2025-11-20 21:31:16 +0500

    Added brackets instead of using slashes

--- app.py
+++ app.py
@@ -30,60 +30,69 @@ customer_df = spark.read.jdbc(url=jdbc_url, table="customer", properties=connect
 address_df = spark.read.jdbc(url=jdbc_url, table="address", properties=connection_props)
 city_df = spark.read.jdbc(url=jdbc_url, table="city", properties=connection_props)
 
-q1_df = film_category_df.join(category_df, on="category_id", how="inner") \
+q1_df = (film_category_df.join(category_df, on="category_id", how="inner")
                         .groupBy("name").count().orderBy("count", ascending=False)
-q2_df = rental_df.join(inventory_df, on="inventory_id", how="inner") \
-                 .join(film_actor_df, on="film_id", how="inner") \
-                 .join(actor_df, on="actor_id", how="inner") \
+        )                 
+q2_df = (rental_df.join(inventory_df, on="inventory_id", how="inner")
+                 .join(film_actor_df, on="film_id", how="inner")
+                 .join(actor_df, on="actor_id", how="inner")
                  .groupBy("actor_id", "first_name", "last_name").count().orderBy("count", ascending=False).limit(10)
-q3_df = payment_df.join(rental_df, on="rental_id", how="inner") \
-                  .join(inventory_df, on="inventory_id", how="inner") \
-                  .join(film_category_df, on="film_id", how="inner") \
-                  .join(category_df, on="category_id", how="inner") \
+        )
+q3_df = (payment_df.join(rental_df, on="rental_id", how="inner")
+                  .join(inventory_df, on="inventory_id", how="inner")
+                  .join(film_category_df, on="film_id", how="inner")
+                  .join(category_df, on="category_id", how="inner")
                   .groupBy("category_id", "name").sum("amount").orderBy("sum(amount)", ascending=False).limit(1)
+        )
 q4_df = film_df.join(inventory_df, on="film_id", how="left_anti").select("title")
-counts_df = actor_df.join(film_actor_df, on="actor_id", how="inner") \
-                    .join(film_category_df, on="film_id", how="inner") \
-                    .join(category_df, on="category_id", how="inner") \
-                    .filter(F.col("name") == "Children").groupBy("actor_id", "first_name", "last_name") \
+counts_df = (actor_df.join(film_actor_df, on="actor_id", how="inner")
+                    .join(film_category_df, on="film_id", how="inner")
+                    .join(category_df, on="category_id", how="inner")
+                    .filter(F.col("name") == "Children").groupBy("actor_id", "first_name", "last_name")
                     .agg(F.count("*").alias("children_movie_count"))
+            )
 w = w.Window.orderBy(F.desc("children_movie_count"))
-q5_df = counts_df.withColumn("rank", F.dense_rank().over(w)) \
-                 .filter(F.col("rank") <= 3) \
+q5_df = (counts_df.withColumn("rank", F.dense_rank().over(w))
+                 .filter(F.col("rank") <= 3)
                  .orderBy(F.desc("children_movie_count"), "last_name", "first_name").select("actor_id", "first_name", "last_name", "children_movie_count")
-q6_df = customer_df.join(address_df, on="address_id", how="inner") \
-                   .join(city_df, on="city_id", how="inner") \
-                   .groupBy("city") \
+        )
+q6_df = (customer_df.join(address_df, on="address_id", how="inner")
+                   .join(city_df, on="city_id", how="inner")
+                   .groupBy("city")
                    .agg(
                        F.sum(F.when(F.col("active") == 1, 1).otherwise(0)).alias("active_customers"),
                        F.sum(F.when(F.col("active") == 0, 1).otherwise(0)).alias("inactive_customers")
-                   ) \
+                   )
                    .orderBy(F.desc("inactive_customers"))
+        )
 rental_hours_df = rental_df.withColumn(
     "rental_hours",
     (F.unix_timestamp("return_date") - F.unix_timestamp("rental_date")) / 3600
 )
-joined_df = rental_hours_df.join(inventory_df, on="inventory_id", how="inner") \
-                           .join(film_df, on="film_id", how="inner") \
-                           .join(film_category_df, on="film_id", how="inner") \
-                           .join(category_df, on="category_id", how="inner") \
-                           .join(customer_df, on="customer_id", how="inner") \
-                           .join(address_df, on="address_id", how="inner") \
+joined_df = (rental_hours_df.join(inventory_df, on="inventory_id", how="inner")
+                           .join(film_df, on="film_id", how="inner")
+                           .join(film_category_df, on="film_id", how="inner")
+                           .join(category_df, on="category_id", how="inner")
+                           .join(customer_df, on="customer_id", how="inner")
+                           .join(address_df, on="address_id", how="inner")
                            .join(city_df, on="city_id", how="inner")
+            )
 a_movies_df = joined_df.filter(F.col("title").startswith("A"))
 dash_cities_df = joined_df.filter(F.col("city").contains("-"))
-a_result = a_movies_df.groupBy("city", "name") \
-                      .agg(F.sum("rental_hours").alias("total_hours")) \
-                      .orderBy(F.desc("total_hours")) \
-                      .groupBy("city") \
+a_result = (a_movies_df.groupBy("city", "name")
+                      .agg(F.sum("rental_hours").alias("total_hours"))
+                      .orderBy(F.desc("total_hours"))
+                      .groupBy("city")
                       .agg(F.first("name").alias("top_category"),
                            F.first("total_hours").alias("hours"))
-dash_result = dash_cities_df.groupBy("city", "name") \
-                            .agg(F.sum("rental_hours").alias("total_hours")) \
-                            .orderBy(F.desc("total_hours")) \
-                            .groupBy("city") \
+            )
+dash_result = (dash_cities_df.groupBy("city", "name")
+                            .agg(F.sum("rental_hours").alias("total_hours"))
+                            .orderBy(F.desc("total_hours"))
+                            .groupBy("city")
                             .agg(F.first("name").alias("top_category"),
                                  F.first("total_hours").alias("hours"))
+              )
 q7_df = a_result.union(dash_result)
 
 q1_df.show()
